fe3e881e46fcf34f2b1dadb66c19baf1654147fb
SACHIN KALKHAMBKAR <90321033+SACHINKALKHAMBKAR@users.noreply.github.com>
2021-09-12 13:09:59 +0530

    Add files via upload

---
+++ Print1/web.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="web.aspx.cs" Inherits="web" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+         <table align="center">
+            <tr>
+                <td id="Name" class="auto-style5">Customer Name</td>
+                <td class="auto-style2">&nbsp;</td>
+                <td id="txtName" class="auto-style3">
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style4">&nbsp;</td>
+            </tr>
+            <tr>
+                <td id="Name" class="auto-style5">Amount</td>
+                <td class="auto-style2">&nbsp;</td>
+                <td id="txtAmount" class="auto-style3">
+                    <asp:TextBox ID="txtAmount" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style4">&nbsp;</td>
+            </tr>
+              <tr>
+                <td id="Name" class="auto-style5">Batch</td>
+                <td class="auto-style2">&nbsp;</td>
+                <td id="txtBatch" class="auto-style3">
+                    <asp:TextBox ID="txtBatch" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style4">&nbsp;</td>
+            </tr>
+              <tr>
+                <td id="Name" class="auto-style5">Mobile Number</td>
+                <td class="auto-style2">&nbsp;</td>
+                <td id="txtMobile_no" class="auto-style3">
+                    <asp:TextBox ID="txtMobile_no" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style4">&nbsp;</td>
+            </tr>
+             <tr>
+             <td class="auto-style1">
+                    <asp:Button ID="btnCreate" runat="server" OnClick="Gen_bill_Click" Text="Submit" />
+                </td>
+                 </tr>
+        </table>
+
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Print1/web.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Text;
+
+
+public partial class web : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Gen_bill_Click(object sender, EventArgs e)
+    {
+        OleDbConnection my_con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\lenovo\Documents\Database1.accdb");
+        my_con.Open();
+        OleDbCommand o_cmd = new OleDbCommand("insert into Cust values(@a,@b,@c,@d,@e)", my_con);
+        o_cmd.Parameters.AddWithValue("a", txtMobile_no.Text);
+        o_cmd.Parameters.AddWithValue("b", txtName.Text);
+        o_cmd.Parameters.AddWithValue("c", txtBatch.Text);
+        o_cmd.Parameters.AddWithValue("d", txtAmount.Text);
+        o_cmd.Parameters.AddWithValue("e", txtMobile_no.Text);
+        int i = o_cmd.ExecuteNonQuery();
+        if (i > 0)
+        {
+            Response.Write(" <script>alert('Data Inserted !!')</script> ");
+        }
+        my_con.Close();
+
+        //** Code to create pdf file
+       // int id = int.Parse((sender as LinkButton).CommandArgument);
+        //byte[] bytes;
+        string fileName, contentType;
+        //string constr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\lenovo\Documents\Database1.accdb";
+            //ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+       /* using (SqlConnection con = new SqlConnection(constr))
+        {
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.CommandText = "select * from Cust where Id=@ID";
+                cmd.Parameters.AddWithValue("@ID", txtMobile_no.Text);
+                cmd.Connection = con;
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    sdr.Read();
+                    bytes = (byte[])sdr["Data"];
+                    contentType = sdr["ContentType"].ToString();
+                    fileName = sdr["Name"].ToString();
+                }
+                con.Close();
+            }
+        }*/
+
+        my_con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\lenovo\Documents\Database1.accdb");
+        my_con.Open();
+        o_cmd = new OleDbCommand("select * from Cust where ID=@ID", my_con);
+        o_cmd.Parameters.AddWithValue("@ID", txtMobile_no.Text);
+        OleDbDataAdapter da = new OleDbDataAdapter(o_cmd);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+       // bytes = (Byte[])dt.Rows[0]["Data"];
+        //bytes = dt
+       // bytes str = dt.Rows[0].ItemArray[0].ToString + dt.Rows[0].ItemArray[1].ToString;
+       // bytes = (Byte[])(str);
+
+       StringBuilder builder = new StringBuilder();
+
+        foreach (DataRow row in dt.Rows)
+        {
+            for (int j = 0; j < row.ItemArray.Length; j++)
+            {
+                string rowText = row.ItemArray[j].ToString();
+                if (rowText.Contains(","))
+                {
+                    rowText = rowText.Replace(",", "/");
+                }
+
+                builder.Append(rowText + ",");
+            }
+            builder.Append(Environment.NewLine);
+        }
+        //Convert to Byte Array 
+        byte[] bytes = Encoding.ASCII.GetBytes(builder.ToString());
+
+        Response.Clear();
+        Response.Buffer = true;
+        Response.Charset = "";
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        Response.ContentType = "application/pdf";
+        Response.AppendHeader("Content-Disposition", "attachment; filename=" + txtMobile_no.Text +".pdf" );
+       Response.BinaryWrite(bytes);
+        Response.Flush();
+        Response.End();
+
+
+
+
+    }
+}
